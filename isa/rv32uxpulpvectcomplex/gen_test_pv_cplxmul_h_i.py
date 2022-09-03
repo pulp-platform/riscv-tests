b77ae187f7cb83327d7057b3dea0cb59b886d95a
Philippe Sauter <phsauter@student.ethz.ch>
2022-09-03 17:26:31 +0200

    Add tests for vectcomplex subset of Xpulpimg
    The tests were automatically generated using pulp_test_gen.
    All include non-exhaustive checks of some common edge cases and
    a few dozen randomly generated tests.

---
+++ isa/rv32uxpulpvectcomplex/gen_test_pv_cplxmul_h_i.py
@@ -0,0 +1,92 @@
+import numpy as np # for easy casting via uint32(), int16() etc
+import os
+import bitstring
+
+import sys
+sys.path.append("../scripts")
+from pulp_test_gen import pulp_test_rrr_op
+
+class pulp_test_pv_cplxmul_h_i(pulp_test_rrr_op):
+    def __init__(self):
+        super().__init__("pv.cplxmul.h.i", res_format  = "0x{:08x}", 
+                                         src1_format = "0x{:08x}",
+                                         src2_format = "0x{:08x}",
+                                         src3_format = "0x{:08x}")
+
+        self.minmax[0] = (0x00000000, +0xFFFFFFFF) # uint32
+        self.minmax[1] = (0x00000000, +0xFFFFFFFF) # uint32
+        self.minmax[2] = (0x00000000, +0xFFFFFFFF) # uint32
+        self.rshift = 15
+        
+        
+    def operation(self, src1: int, src2: int, src3: int) -> int:
+        bitstring.set_lsb0(True)
+        bits1 = bitstring.pack('uint:32', src1)
+        bits2 = bitstring.pack('uint:32', src2)
+        res = bitstring.pack('uint:32', src3)
+        
+        op1_real = bits1[ 0:16].unpack('int:16')[0]
+        op1_imag = bits1[16:32].unpack('int:16')[0]
+        op2_real = bits2[ 0:16].unpack('int:16')[0]
+        op2_imag = bits2[16:32].unpack('int:16')[0]
+
+        # Im{res} = (Re{op1}*Im{op2} - Im{op1}*Re{op2}) >> rshift
+        res_imag = op1_real*op2_imag + op1_imag*op2_real
+        imag_bits = bitstring.pack('int:33', res_imag)
+        imag_bits = imag_bits[0:32]
+        imag_bits.int >>= self.rshift
+        res[16:32] = imag_bits[0:16]
+        
+        return res.uint
+
+
+if __name__ == '__main__':
+    pv_cplxmul_h_i = pulp_test_pv_cplxmul_h_i()
+    pv_cplxmul_h_i.file_path = os.path.join(".", pv_cplxmul_h_i.mnemonic.replace('.', '_') + ".S")
+
+    pv_cplxmul_h_i.add_arith_test(0x55667788, 0x11223344, 0x12345678)
+    pv_cplxmul_h_i.add_arith_test(0x11223344, 0x55667788, 0x12345678)
+    pv_cplxmul_h_i.add_arith_test(0x80008000, 0x80008000, 0x12345678)
+    pv_cplxmul_h_i.add_arith_test(0x7fff8000, 0x80007fff, 0x12345678)
+    pv_cplxmul_h_i.gen_all_tests(15, 2)
+    pv_cplxmul_h_i.write_asm()
+
+
+    pv_cplxmul_h_i_div2 = pulp_test_pv_cplxmul_h_i()
+    pv_cplxmul_h_i_div2.mnemonic += '.div2'
+    pv_cplxmul_h_i_div2.rshift = 16
+    pv_cplxmul_h_i_div2.file_path = os.path.join(".", pv_cplxmul_h_i_div2.mnemonic.replace('.', '_') + ".S")
+
+    pv_cplxmul_h_i_div2.add_arith_test(0x55667788, 0x11223344, 0x12345678)
+    pv_cplxmul_h_i_div2.add_arith_test(0x11223344, 0x55667788, 0x12345678)
+    pv_cplxmul_h_i_div2.add_arith_test(0x80008000, 0x80008000, 0x12345678)
+    pv_cplxmul_h_i_div2.add_arith_test(0x7fff8000, 0x80007fff, 0x12345678)
+    pv_cplxmul_h_i_div2.gen_all_tests(15, 2)
+    pv_cplxmul_h_i_div2.write_asm()
+
+    
+    pv_cplxmul_h_i_div4 = pulp_test_pv_cplxmul_h_i()
+    pv_cplxmul_h_i_div4.mnemonic += '.div4'
+    pv_cplxmul_h_i_div4.rshift = 17
+    pv_cplxmul_h_i_div4.file_path = os.path.join(".", pv_cplxmul_h_i_div4.mnemonic.replace('.', '_') + ".S")
+
+    pv_cplxmul_h_i_div4.add_arith_test(0x55667788, 0x11223344, 0x12345678)
+    pv_cplxmul_h_i_div4.add_arith_test(0x11223344, 0x55667788, 0x12345678)
+    pv_cplxmul_h_i_div2.add_arith_test(0x80008000, 0x80008000, 0x12345678)
+    pv_cplxmul_h_i_div4.add_arith_test(0x7fff8000, 0x80007fff, 0x12345678)
+    pv_cplxmul_h_i_div4.gen_all_tests(15, 2)
+    pv_cplxmul_h_i_div4.write_asm()
+
+    
+    pv_cplxmul_h_i_div8 = pulp_test_pv_cplxmul_h_i()
+    pv_cplxmul_h_i_div8.mnemonic += '.div8'
+    pv_cplxmul_h_i_div8.rshift = 18
+    pv_cplxmul_h_i_div8.file_path = os.path.join(".", pv_cplxmul_h_i_div8.mnemonic.replace('.', '_') + ".S")
+
+    pv_cplxmul_h_i_div8.add_arith_test(0x55667788, 0x11223344, 0x12345678)
+    pv_cplxmul_h_i_div8.add_arith_test(0x11223344, 0x55667788, 0x12345678)
+    pv_cplxmul_h_i_div2.add_arith_test(0x80008000, 0x80008000, 0x12345678)
+    pv_cplxmul_h_i_div8.add_arith_test(0x7fff8000, 0x80007fff, 0x12345678)
+    pv_cplxmul_h_i_div8.gen_all_tests(15, 2)
+    pv_cplxmul_h_i_div8.write_asm()
+
